e1dc7d003e8d3eee335625a5fcb6f7ab96afeb75
Johnny Sparks <johnnyfuchs@gmail.com>
2015-03-26 15:39:45 -0700

    Retry cleverbot

---
+++ scripts/cleverbot.js
@@ -0,0 +1,13 @@
+var Cleverbot = require('cleverbot-node')
+var silly = require('../lib/silly')
+var matchLast = require('../lib/matchLast')
+
+module.exports = function(robot) {
+  var cleverbot = new Cleverbot();
+  matchLast(robot,function(msg){
+    silly(msg)
+    cleverbot.write(msg.match[1] + msg.match[2], function(a){
+      msg.send(a.message);
+    });
+  });
+};
